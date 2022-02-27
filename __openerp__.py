eaaf8a967ab57b806aa6feda7fad5f2eccbc9f98
EgyCrash <egy.crash@gmail.com>
2022-02-27 10:57:00 +0200

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-###############################################################################
+##############################################################################
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-today OpenERP SA (<http://www.openerp.com>)
@@ -18,7 +18,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-###############################################################################
+##############################################################################
 
 
 {
